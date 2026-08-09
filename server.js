c95bd0d7ca7e318dd14bd83fea55f56e6ce73573
kekekoby <jacobaluna8@gmail.com>
2026-08-09 17:56:20 +0800

    feat: added session middleware

--- server.js
+++ server.js
@@ -2,7 +2,22 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
+const session = require('express-session');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(
+  session({
+	secret: process.env.SESSION_SECRET,
+	resave: false,
+	saveUninitialized: false,
+	cookie: { maxAge: 1000 * 60 * 60 * 24 * 7 }, // 1 week cookie
+  })
+);
+
+const path = require('path');
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.listen(PORT, () => {
-    console.log(`Server running at http://localhost:${PORT}`);
+	console.log(`Server running at http://localhost:${PORT}`);
 });
